5756bbd27f946bd8f2ee3195ae60334f0e57d151
pp <pyang33@illinois.edu>
2026-03-17 12:10:27 -0500

    fix: frontend page

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Youtube-Transcript",
+  basePath: isProd ? "/Youtube-Transcript" : "",
+  assetPrefix: isProd ? "/Youtube-Transcript/" : "",
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
